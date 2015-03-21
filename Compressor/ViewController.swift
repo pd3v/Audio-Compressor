188cafe2cd2ce9a7f5417e7219158b50fcbc5d62
paulodevelop <paulo.develop@gmail.com>
2015-03-21 11:16:33 +0000

    Transforming AudioINOUT project into NoiseGate project

--- Compressor/ViewController.swift
+++ Compressor/ViewController.swift
@@ -1,17 +1,11 @@
-//
-//  ViewController.swift
-//  SwiftHarmonizer
-//
-//  Created by Nicholas Arner on 10/7/14.
-//  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    let compressor = CompressorInstrument();
-    let sampler = AKSampler();
+    @IBOutlet weak var bttOnOff: UIButton!
+    
+    let compressor = CompressorInstrument()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +13,8 @@ class ViewController: UIViewController {
         AKOrchestra.addInstrument(compressor)
         AKOrchestra.start()
         
-        compressor.play()
+        //println(aA.trackedAmplitude)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,21 +22,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func startRecording(sender: AnyObject) {
-        compressor.play()
-        sampler.startRecordingToTrack("harmonizer")
+    @IBAction func toggleCompressing(sender: UIButton) {
+        if (sender.currentTitle == "Off") {
+            compressor.play()
+            //compressor.buildUp()
+            sender.setTitle("On", forState: .Normal)
+        } else {
+            compressor.stop()
+            //println(compressor.audioAmplitude!.trackedAmplitude)
+            sender.setTitle("Off", forState: .Normal)
+        }
     }
     
-    @IBAction func stopRecording(sender: AnyObject) {
-        compressor.stop()
-        sampler.stopRecordingToTrack("harmonizer")
-    }
-    
-    @IBAction func startPlaying(sender: AnyObject) {
-        sampler.startPlayingTrack("harmonizer")
-    }
-
-    @IBAction func stopPlaying(sender: AnyObject) {
-        sampler.stopPlayingTrack("harmonizer")
-    }
 }
