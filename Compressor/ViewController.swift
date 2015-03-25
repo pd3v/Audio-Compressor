df3e6848825e8c35b80304dfb9d216729966a99c
paulodevelop <paulo.develop@gmail.com>
2015-03-25 12:52:42 +0000

    Interface with main properties (threshold, compressionRatio, attackTime, releaseTime) added.

--- Compressor/ViewController.swift
+++ Compressor/ViewController.swift
@@ -4,6 +4,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var bttOnOff: UIButton!
+    @IBOutlet weak var sldThreshold: UISlider!
+    @IBOutlet weak var sldCompressionRatio: UISlider!
+    @IBOutlet weak var sldAttackTime: UISlider!
+    @IBOutlet weak var sldReleaseTime: UISlider!
+    
+    @IBOutlet weak var lblThreshold: UILabel!
+    @IBOutlet weak var lblCompRatio: UILabel!
+    @IBOutlet weak var lblAttackTime: UILabel!
+    @IBOutlet weak var lblReleaseTime: UILabel!
     
     let compressor = CompressorInstrument()
     
@@ -14,24 +23,61 @@ class ViewController: UIViewController {
         AKOrchestra.start()
         
         //println(aA.trackedAmplitude)
-
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        lblThreshold.text = String(format:"%.f dB", compressor.threshold.value)
+        lblCompRatio.text = String(format:"%.f:1", compressor.compRatio.value)
+        lblAttackTime.text = String(format:"%.1f sec", compressor.attackTime.value)
+        lblReleaseTime.text = String(format:"%.1f sec", compressor.releaseTime.value)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        AKOrchestra.reset()
+        //AKManager.sharedManager().stop() //? Is object necessary if there sound rounting singleton?
     }
 
     @IBAction func toggleCompressing(sender: UIButton) {
         if (sender.currentTitle == "Off") {
             compressor.play()
-            //compressor.buildUp()
             sender.setTitle("On", forState: .Normal)
+            
+            //! Instead of enabling each one, could do loop through all sliders!
+            sldCompressionRatio.enabled = true
+            sldThreshold.enabled = true
+            sldAttackTime.enabled = true
+            sldReleaseTime.enabled = true
+            
         } else {
             compressor.stop()
-            //println(compressor.audioAmplitude!.trackedAmplitude)
             sender.setTitle("Off", forState: .Normal)
+            
+            //! Instead of desabling each one, could do loop through all sliders!
+            sldCompressionRatio.enabled = false
+            sldThreshold.enabled = false
+            sldAttackTime.enabled = false
+            sldReleaseTime.enabled = false
         }
     }
     
+    @IBAction func thresholdChanged(sender: UISlider) {
+        AKTools.setProperty(compressor.threshold, withSlider: sender)
+        lblThreshold.text = String(format:"%d dB", Int(sender.value))
+    }
+
+    @IBAction func compressionRatioChanged(sender: UISlider) {
+        AKTools.setProperty(compressor.compRatio, withSlider: sender)
+        lblCompRatio.text = String(format:"%.f:1", sender.value)
+    }
+    
+    @IBAction func attackTimeChanged(sender: UISlider) {
+        AKTools.setProperty(compressor.attackTime, withSlider: sender)
+        lblAttackTime.text = String(format:"%.3f sec", sender.value)
+    }
+    
+    @IBAction func releaseTimeChanged(sender: UISlider) {
+        AKTools.setProperty(compressor.releaseTime, withSlider: sender)
+        lblReleaseTime.text = String(format:"%.1f sec", sender.value)
+    }
 }
